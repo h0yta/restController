04299c4f77a4e2169c41b2decceca4dd7c152bca
h0yta <oscar@vettig.se>
2018-07-30 23:16:35 +0200

    Finished download integration

--- src/main/java/se/tornroth/airdc/entity/Download.java
+++ src/main/java/se/tornroth/airdc/entity/Download.java
@@ -3,19 +3,28 @@ package se.tornroth.airdc.entity;
 public class Download {
 	private final String title;
 	private final String season;
-	private final String episode;
+	private final String fromEpisode;
+	private final String toEpisode;
 
 	public Download(String title) {
 		this.title = title;
 		this.season = null;
-		this.episode = null;
+		this.fromEpisode = null;
+		toEpisode = null;
 	}
 
 	public Download(String title, String season, String episode) {
-		super();
+		this.title = title.trim();
+		this.season = season.trim();
+		this.fromEpisode = episode.trim();
+		this.toEpisode = null;
+	}
+
+	public Download(String title, String season, String fromEpisode, String toEpisode) {
 		this.title = title;
 		this.season = season;
-		this.episode = episode;
+		this.fromEpisode = fromEpisode;
+		this.toEpisode = toEpisode;
 	}
 
 	public String getTitle() {
@@ -26,13 +35,19 @@ public class Download {
 		return season;
 	}
 
-	public String getEpisode() {
-		return episode;
+	public String getFromEpisode() {
+		return fromEpisode;
+	}
+
+	public String getToEpisode() {
+		return toEpisode;
 	}
 
 	public String getSearchString() {
-		if (season != null && episode != null) {
-			return title + " " + season + "x" + episode;
+		if (season != null && fromEpisode != null && toEpisode != null) {
+			return title + " " + season + "x" + fromEpisode + "-" + toEpisode;
+		} else if (season != null && fromEpisode != null) {
+			return title + " " + season + "x" + fromEpisode;
 		}
 
 		return title;
